00b147923276694fa1753e4cefe36a0a7e75e791
Your NameTomRiej <Tom8434@dubaicollege.org>
2020-09-29 09:14:28 +0400

    Update Credit.py
    V7 Added GenerateCC() function and added comments.

--- Credit.py
+++ Credit.py
@@ -4,9 +4,13 @@
 from random import randint
 
 CARD_LENGTH = 16
+WRITE_PATH = "/Users/Tom/Desktop/CS-A-Level/CreatedCCs.txt"
+
+# Path specific to me:
+# /Users/Tom/Desktop/CS-A-Level/MWDCredit.txt
 
 
-# ================= Luhn's algoritm and Validation Subroutines ===========
+# ======================= Validation Subroutines =========================
 
 def validateFormat(cardNum):
     """checks a string of numbers separated by spaces if it is 16 numbers long
@@ -56,12 +60,20 @@ def validateCardNumber(cardNum):
             sumESN += ix2
     
     total = sumESN + sum(cardNum)
-    if str(total)[-1] == "0":
+    if str(total)[-1] == "0": # fix % 10
         return True
     else:
         return False
 
 def validatePath(path):
+    """validates path given by user
+
+    Args:
+        path (String): The path
+
+    Returns:
+        Bool: True if valid
+    """
     try:
         fileHandle = open(path,"r")
     except:
@@ -69,12 +81,16 @@ def validatePath(path):
     fileHandle.close()
     return True
 
-def createValidCC():
-    # max : 9999999999999999 => (with luhn alg.) total of 144 ==> highest valid num total is 140
-    # min : 0000000000000000 => (with luhn alg.) total of 0  ==> lowest valid sum is 0 
 
-    # Creates every Second number, as these are the ones which undergo luhn alg
-    sumMaxSingle = 9*8
+# ======================= Decomposition subroutines ======================
+
+def createESN():
+    """Creates a random list of 8 numbers which then undergo luhns algorithm
+
+    Returns:
+        [int]: list of random integers
+        (int): sum of the integers after luhn's algorithm is implemented.
+    """
     eSN = []
     sumESN = 0
     for i in range(0,CARD_LENGTH//2):
@@ -85,39 +101,55 @@ def createValidCC():
             sumESN += res - 9 
         else:
             sumESN += res
-        
-    # created a maximum and minimum valid total number (last digit is 0)
-    sumMax = int(str(sumESN + sumMaxSingle)[:-1]) # removes the last digit to make RNG*10 make a valid total (0 at end) 
-    sumMin = int(str(sumESN+10)[:-1])             
-    total = randint(sumMin,sumMax)*10
-    difference = total - sumESN
 
-    # the difference is then made up using random numbers
-    # these numbers become the other half of every second number to create a full card number
-    unalteredDigits = []
-    while len(unalteredDigits) < 8:
-        if difference > 0:
-            digit = randint(0,9)
-            if difference - digit >= 0:
-                unalteredDigits.append(digit)
-                difference -= digit
-        else:
-            unalteredDigits.append(0)
-    
+    return eSN, sumESN
 
-    # puts both halves of the card number together
-    validCardNum = []
+def zipLists(list1, list2):
+    """puts 2 lists together
+
+    Args:
+        list1 [any]: A list of items, index 0 becomes index 0 in zipped list
+        list2 [any]: A list of items, index 0 becomes index 1 in zipped list
+
+    Returns:
+        [any]: The final zipped list
+    """
+    endList = []
     for i in range(CARD_LENGTH//2):
-        validCardNum.append(eSN[i])
-        validCardNum.append(unalteredDigits[i])
-    return "".join([str(i) for i in validCardNum])
-    
-    
+        endList.append(list1[i])
+        endList.append(list2[i])
+    return endList
 
+def createValidCC():
+    """Creates 1 valid card number
+    First creates every second number and finds the sum after Luhn's algorithm
+    Second creates 7 random digits for the unaltered numbers
+    The difference to a valid total is then added as the 8th
+    Lastly zips the lists together to get valid card number.
 
+    Returns:
+        (String): The valid card number.
+    """
 
+    # Creating a random 8 numbers and finding the result after Luhn's algorithm
+    eSN, sumESN = createESN()
+
+    # Creatign another 7 random numbers (unaltered digits in Luhn's algorithm)
+    uAN = [randint(0,9) for i in range(CARD_LENGTH//2-1)]
+
+    total = sum(uAN) + sumESN
+    rtotal = round((total+5)/10)*10 # rounding up to nearest 10
+    difference = rtotal - total
+    if difference != 10:
+        uAN.append(difference)
+    else:
+        uAN.append(0)
     
-    
+    # puts both halves of the card number together
+    validCardNum = zipLists(eSN, uAN)
+    validCardNum = "".join([str(i) for i in validCardNum])
+    return validCardNum
+
 
 # ========================= Subroutines for the 3 options ================
 
@@ -135,7 +167,6 @@ def checkCard():
             print("The card Number is valid! ")
         else:
             print("the card Number is invalid...")
-
 
 def checkImportNum():
     """Called from the main subroutine
@@ -161,9 +192,11 @@ def checkImportNum():
     else:
         print("Invalid path")
 
-
-
 def generateCC():
+    """Generates x amount of valid, unduplicated credit card numbers
+    asks and validates for the quantity of numbers the user wants.
+    Writes the created numbers into a file at a specified location
+    """
     valid = False
     while not valid:
         try:
@@ -175,20 +208,28 @@ def generateCC():
         except:
             print("Invalid input: must be a number from 1-100")
 
-    for i in range(0,quantity):
-        validCardNum = createValidCC()
-        #  Tests:
-        # tempCardNum, boo = validateFormat(validCardNum)
-        # print(validCardNum,"Format: ",boo,"Number:", validateCardNumber(tempCardNum))
-
+    madeNumbers = []
+    while len(madeNumbers) < quantity:
+        newNum = createValidCC()
+        if newNum not in madeNumbers:
+            madeNumbers.append(newNum)
+        else:
+            print(madeNumbers,"\nduplicate:",newNum)
+    
+    with open(WRITE_PATH, "w") as f:
+        for i in madeNumbers:
+            print(i)
+            f.write(i+"\n")
+    f.close()
+    print("Numbers written to file at:", WRITE_PATH)
 
 
 # ========================= Menu and main Program ========================
 def menu():
-    """Displays the menu for the user
+    """Shows the menu to the user
 
     Returns:
-        (int): valid user input
+        int: the users choice
     """
     print("\n\nOptions:")
     print("1. Check my card")
@@ -207,15 +248,14 @@ def menu():
             print("invalid input: must be integer from 1-4\n")
     return ans
 
-def main(ans):
-    """Main subroutine
-        calls subroutines according to users choice
+def pickFunc(ans):
+    """Calls the function corresponding to the users input
 
     Args:
-        ans (int): users choice, subrouting called accordingly
+        ans (int): the users choice
 
     Returns:
-        (Bool): False to quit the main loop
+        bool: True to continue the program, False to end the 'go' while loop
     """
     if ans == 1:
         checkCard()
@@ -232,23 +272,17 @@ def main(ans):
             return False
     return True
 
+def main():
+    """Everything should run through here: the main program
+    """
+    go = True
+    while go:
+        # could do go = pickFunc(menu()) but for simplicity:
+        chosen = menu()
+        go = pickFunc(chosen)
 
 
+# ========================================================================
 
-
-
-go = True
-while go:
-    # Could just do: go = main(menu()) but for clarity:
-    chosen = menu()
-    go = main(chosen)
-
-
-# testCardNum = "0378282246310005"
-# num, boo = validateFormat(testCardNum)
-# print(validateCardNumber(num))
-
-# checkImportNum()
-
-# Path specific to me:
-# /Users/Tom/Desktop/CS-A-Level/MWDCredit.txt
+if __name__ == "__main__":
+    main()
